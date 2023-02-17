c28ac2fd902ea97babb9f92c8bec2b4a31a12df6
Muhammed Orhun Gale <morhun@sabanciuniv.edu>
2023-02-17 12:24:14 +0300

    Tournament user progress added

--- src/main/java/com/dreamGames/rowMatchBackend/controller/UserController.java
+++ src/main/java/com/dreamGames/rowMatchBackend/controller/UserController.java
@@ -6,6 +6,7 @@ import com.dreamGames.rowMatchBackend.responses.AuthenticationResponse;
 import com.dreamGames.rowMatchBackend.responses.CreateUserResponse;
 import com.dreamGames.rowMatchBackend.responses.OneLevelProgressResponse;
 import com.dreamGames.rowMatchBackend.security.JWTTokenProvider;
+import com.dreamGames.rowMatchBackend.service.TournamentService;
 import com.dreamGames.rowMatchBackend.service.UserDetailsServiceImplementation;
 import com.dreamGames.rowMatchBackend.service.UserProgressService;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -29,26 +30,13 @@ import java.util.*;
 @RequiredArgsConstructor
 public class UserController {
     private final UserProgressService userService;
+    private final TournamentService tournamentService;
     private final AuthenticationManager authenticationManager;
     private final PasswordEncoder passwordEncoder;
     private final JWTTokenProvider JWTTokenProvider;
     private final UserDetailsServiceImplementation userDetailsServiceImplementation;
 
-    @PostMapping("/auth")
-    public ResponseEntity<?> auth(@Valid @RequestBody UserRequest userRequest) throws Exception {
-        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userRequest.getUsername(), userRequest.getPassword());
-        Authentication auth = authenticationManager.authenticate(authToken);
-        SecurityContextHolder.getContext().setAuthentication(auth);
-        String jwtToken = JWTTokenProvider.generateJWTToken(auth);
-
-        AuthenticationResponse response = new AuthenticationResponse();
-        response.setJwtToken(jwtToken);
-        response.setUsername(userRequest.getUsername());
-
-        return new ResponseEntity<AuthenticationResponse>(response, HttpStatus.CREATED);
-    }
-
-    @PostMapping("/create")
+    @PostMapping("/CreateUserRequest")
     public ResponseEntity<?> create(@Valid @RequestBody UserRequest userRequest) {
         User user = userService.createUser(userRequest.getUsername(), passwordEncoder.encode(userRequest.getPassword()));
 
@@ -60,15 +48,34 @@ public class UserController {
         return new ResponseEntity<CreateUserResponse>(response, HttpStatus.CREATED);
     }
 
-    @PostMapping("/progress")
+    @PostMapping("/UpdateLevelRequest")
     public ResponseEntity<?> progress() {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
         User updatedUser = userService.oneLevelUserProgress(user).get();
+        try{
+            Boolean x = tournamentService.updateUserActiveTournamentRanking(user);
+            System.out.println(x);
+        }
+        catch (RuntimeException e) {System.out.println(e);}
         OneLevelProgressResponse response = new OneLevelProgressResponse();
         response.setLevel(updatedUser.getCurrentLevel());
         response.setCoin(updatedUser.getCurrentCoins());
 
         return new ResponseEntity<OneLevelProgressResponse>(response, HttpStatus.OK);
+    }
+
+    @PostMapping("/AuthenticateUserRequest")
+    public ResponseEntity<?> auth(@Valid @RequestBody UserRequest userRequest) throws Exception {
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userRequest.getUsername(), userRequest.getPassword());
+        Authentication auth = authenticationManager.authenticate(authToken);
+        SecurityContextHolder.getContext().setAuthentication(auth);
+        String jwtToken = JWTTokenProvider.generateJWTToken(auth);
+
+        AuthenticationResponse response = new AuthenticationResponse();
+        response.setJwtToken(jwtToken);
+        response.setUsername(userRequest.getUsername());
+
+        return new ResponseEntity<AuthenticationResponse>(response, HttpStatus.CREATED);
     }
 }
